7409e1ee90ea3a708cb23e1698d7c824789cf01f
Dev User <dev-user+00000000000000000000000000000001@appgenly.com>
2025-11-03 21:35:33 +0300

    Initial commit - demo project template

---
+++ projects/00000000-0000-0000-0000-000000000001_demo-landing-45081856e5114187b257f25b27725c24/src/App.tsx
@@ -0,0 +1,10 @@
+import { Index } from './pages/Index'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <Index />
+    </div>
+  )
+}
+
